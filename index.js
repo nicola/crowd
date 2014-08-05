0b2eed61f7fdb41fe279149c85e7e79805145db9
nicolagreco <notsecurity@gmail.com>
2014-08-05 03:22:08 -0700

    first version with event emitter

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-module.exports = require("./lib");
+module.exports = Crowd
+
+var EventEmitter = require('events').EventEmitter
+var util = require('util')
+
+/**
+ * Create a new crowd task
+ *
+ *
+ * @override
+ * @param {string|Buffer|Object} torrentId torrent (choose from above list)
+ * @param {Object}        opts      optional torrent-specific options
+ * @param {function=}     cb        called when the torrent is ready and has metadata
+ */
+function Crowd () {
+  var args = arguments
+  this.task = undefined
+  this.data = {}
+  this.opts = {}
+
+  if (typeof args[0] == 'function' && typeof args[1] != 'function' ) {
+    this.task = args[0]
+    if (args[1]) this.opts = args[1]
+  } else {
+    this.data = args[0]
+    this.task = args[1]
+    if (args[2]) this.opts = args[2]
+  }
+
+  if (this.opts.limit) {
+    this.numRequests = 0
+    this.on('data', function (stream) {
+      this.numRequests++;
+    });
+  }
+}
+util.inherits(Crowd, EventEmitter);
+
+Crowd.prototype.toJSON = function() {
+  var sending = {}
+
+  if (this. data) sending.data = JSON.stringify(this.data),
+  sending.task = this.task.stringify()
+
+  return task
+}
+
+Crowd.prototype.handleResult = function(result) {
+  this.emit('data', result)
+  if (this.opts.limit >= this.numRequests) {
+    this.emit('end', result)
+  }
+}
